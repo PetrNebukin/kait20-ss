bdc107c54abf5d9dd29fb9b4b1dbb94b085e92fb
Petr Nebukin <69680236+PetrNebukin@users.noreply.github.com>
2024-04-03 08:56:01 +0000

    new version; changes to index.html, script.js, style.css; should add about page

--- script.js
+++ script.js
@@ -1,27 +1,51 @@
-document.addEventListener('mousemove', e => {
-	const x = e.clientX;
-	const y = e.clientY;
-    document.body.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(154,68,141, 1) 10%, rgba(33, 37, 41, 1))`;
-});
+const textcontainer = document.querySelector('div[class="text-container"]');
+const nextbutton = document.querySelector('button[name="continue"]');
+const readbutton = document.querySelector('button[name="read"]');
 
-document.addEventListener('mouseleave', () => {
-    document.body.style.background = 'rgba(33, 37, 41, 1)';
-});
+readbutton.style.display = 'none';
+readbutton.style.margin = '0 auto';
+
+// Если useragent содержит Android или iPhone, то добавляем класс mobile к body, иначе добавляем обработчики событий mousemove и mouseleave
+if (/(android|iphone)/i.test(navigator.userAgent)) {
+    document.body.classList.add('mobile');
+}
+else {
+    document.addEventListener('mousemove', e => {
+        const x = e.clientX;
+        const y = e.clientY;
+        document.body.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(154,68,141, 1) 10%, rgba(33, 37, 41, 1))`;
+    });
+    
+    document.addEventListener('mouseleave', () => {
+        document.body.style.background = 'rgba(33, 37, 41, 1)';
+    });
+}
+
+function readMainText() {
+    setTimeout(() => {
+        textcontainer.classList.add('fade-out');
+        window.location.href='/about.html';
+    }, 1000);
+}
 
 function changeContent() {
-    const h1 = document.querySelector('h1');
-    const p = document.querySelector('p');
-    const button = document.querySelector('button');
-    const logo = document.querySelector('img');
+    textcontainer.classList.add('fade-out');
+    setTimeout(() => {
+        const h1 = document.querySelector('h1');
+        const p = document.querySelector('p');
+        const logo = document.querySelector('img');
 
-    h1.textContent = 'КАИТ №20 - настоящая контора пидорасов';
-    p.textContent = 'Колледж автоматизации и информационных технологий №20 - это место, где ты ахуеешь от тупости местного руководства. Преподаватели массово увольняются, а студенты отчисляются. Все это из-за того, что директор колледжа - настоящий пидорас, как и его приближенные.';
+        h1.textContent = 'КАИТ №20 - настоящая контора пидорасов';
+        p.textContent = 'Колледж автоматизации и информационных технологий №20 - это место, где ты ахуеешь от тупости местного руководства. Преподаватели массово увольняются, а студенты отчисляются. Все это из-за того, что директор колледжа - настоящий пидорас, как и его приближенные.';
 
-    button.style.display = 'none';
-    logo.style.display = 'none';
-    h1.style.textAlign = 'center';
-    p.style.textAlign = 'center';
+        nextbutton.style.display = 'none';
+        readbutton.style.display = 'block';
+        logo.style.display = 'none';
 
-    h1.classList.add('fade-in');
-    p.classList.add('fade-in');
+        h1.style.textAlign = 'center';
+        p.style.textAlign = 'center';
+
+        textcontainer.classList.remove('fade-out');
+        textcontainer.classList.add('fade-in');
+    }, 500);
 }
