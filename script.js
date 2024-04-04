6e6d195b694be4dce52c10c5996569d1bc2e18d7
Petr Nebukin <69680236+PetrNebukin@users.noreply.github.com>
2024-04-04 09:11:07 +0000

    added about

--- script.js
+++ script.js
@@ -5,20 +5,23 @@ const readbutton = document.querySelector('button[name="read"]');
 readbutton.style.display = 'none';
 readbutton.style.margin = '0 auto';
 
-// Если useragent содержит Android или iPhone, то добавляем класс mobile к body, иначе добавляем обработчики событий mousemove и mouseleave
+// Если useragent содержит Android или iPhone, то добавляем класс mobile к body, иначе добавляем обработчики событий mousemove и mouseleave, иначе если страница about.html, то добавляем класс fade-in к div с классом image-container
 if (/(android|iphone)/i.test(navigator.userAgent)) {
     document.body.classList.add('mobile');
 }
 else {
-    document.addEventListener('mousemove', e => {
-        const x = e.clientX;
-        const y = e.clientY;
-        document.body.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(154,68,141, 1) 10%, rgba(33, 37, 41, 1))`;
-    });
-    
-    document.addEventListener('mouseleave', () => {
-        document.body.style.background = 'rgba(33, 37, 41, 1)';
-    });
+    console.log(window.location.pathname);
+    if (window.location.pathname === '/about.html') {
+        const imagecontainer = document.querySelector('div[class="image-container"]');
+        imagecontainer.classList.add('fade-in');
+    }
+    else {
+        document.addEventListener('mousemove', e => {
+            const x = e.clientX;
+            const y = e.clientY;
+            document.body.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(154,68,141, 1) 10%, rgba(33, 37, 41, 1))`;
+        });
+    }
 }
 
 function readMainText() {
